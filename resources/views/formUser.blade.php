395ebf9217abf1cab97f045759fee97103cf0c29
Marcelo Staudt <marcelo.121205@bento.local>
2023-08-15 16:55:54 -0300

    Inserção das Views

---
+++ resources/views/formUser.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <form action="">
+        <label>Nome de usuário</label>
+        <input type="text">
+        <br>
+        <label>E-mail</label>
+        <input type="email">
+        <br>
+        <label>Senha</label>
+        <input type="password">
+        <input type="submit">
+    </form>
+</body>
+</html>
