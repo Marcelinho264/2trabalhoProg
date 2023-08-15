6e0cd3899cff5617854458011a99ce268605cdc6
Marcelo Staudt <marcelo.121205@bento.local>
2023-08-15 17:14:00 -0300

    rotas e views

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,30 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::prefix('/usuario')->middleware('auth')->group(function () {
+    Route::get('', [UsuariosController::class, 'index'])->name('usuarios');
+
+    Route::get('view', [UsuariosController::class, 'view'])->name('usuarios.view');
+});
+
+
+Route::prefix('/adm')->middleware('auth')->group(function () {
+    Route::post('/', [ProdutosController::class, 'index']);
+
+    Route::get('/add', [ProdutosController::class, 'add'])->name('adm.add');
+
+    Route::post('/add', [ProdutosController::class, 'addSave'])->name('adm.addSave');
+
+    Route::get('/{filme}', [ProdutosController::class, 'view'])->name('adm.view');
+
+    Route::get('/edit/{filme}', [ProdutosController::class, 'edit'])->name('adm.edit');
+
+    Route::post('/edit/{filme}', [ProdutosController::class, 'editSave'])->name('adm.editSave');
+
+    Route::get('/delete/{filme}', [ProdutosController::class, 'delete'])->name('adm.delete');
+
+    Route::delete('/delete/{filme}', [ProdutosController::class, 'deleteForReal'])->name('adm.deleteForReal');
+
+
+});
