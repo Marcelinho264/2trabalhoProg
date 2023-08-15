fe2adb65fba1fa1d6d47c7249cbb43088668f71e
Pedro Henrique Bock <pedrohenriquecbock@gmail.com>
2023-08-15 16:57:01 -0300

    fix: ponto e vírgula que faltava

--- app/Http/Controllers/UsuariosController.php
+++ app/Http/Controllers/UsuariosController.php
@@ -6,7 +6,6 @@ use Illuminate\Auth\Events\Registered;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use App\Models\Usuario;
-use App\Models\Aluno;
 
 class UsuariosController extends Controller
 {
@@ -28,6 +27,6 @@ class UsuariosController extends Controller
 
         event(new Registered($usuario));
 
-        return redirect()->route('')->with('success', 'Usuario cadastrado com sucesso')
+        return redirect()->route('')->with('success', 'Usuario cadastrado com sucesso');
     }
 }
